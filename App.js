ba28a1ea19eba924acb898d69b526b30a4e59262
Davi Freitas <92342116+daviifreitas@users.noreply.github.com>
2022-12-10 16:42:29 -0300

    .

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert , Button} from 'react-native';
+import api from "../my-app/src/services/api"
 
 export default function App() {
+
+  async function consulta(){
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    console.log(data); 
+    console.log(data.slip);
+    console.log(data.slip.advice);
+    return toString(data.slip.advice);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Clique e se surpreenda-se!" onPress={() => Alert.alert( "Hehe",consulta())}/>
       <StatusBar style="auto" />
     </View>
   );
